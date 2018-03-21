cfef6e0a53ee9f950c6b401745b7cd5f670f4d6d
Eugene <eugenetikk@gmail.com>
2018-03-22 00:26:15 +0300

    added coursework

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp'); 
 var browserSync = require('browser-sync').create();
 
-//Задача умолчанию, перед которой выполнится задача ['webserver']:
-gulp.task('default',['build', 'browser-sync'], function () {
-    gulp.watch('dev/**/*', ['build']).on('change', browserSync.reload);
+//Задача умолчанию, перед которой выполнится 
+//задача 'Запуск сервера'('browser-sync'):
+gulp.task('default',['browser-sync'], function () {
+    gulp.watch('site/**/*', ['build'])
+    .on('change', browserSync.reload); //Refresh
 });
+
 //Задача 'build':
 gulp.task('build', function () {
     return gulp.src('site/**/*')
            .pipe(gulp.dest('../public'));
 });
 
+//Новая задача 'Запуск сервера'('browser-sync'):
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
